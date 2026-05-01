3f3bcab6409da7e07adca281ab233523ad55091d
sani-yl <saniyonjanlama54@gmail.com>
2026-05-02 03:57:17 +0545

    fix express 5 wildcard route

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.delete('/api/persons/:id', (req, res) => {
 })
 
 app.use(express.static(path.join(__dirname, 'dist')))
-app.get('*', (req, res) => {
+app.get('/*splat', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 })
 
