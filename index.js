431ee6e3092e9b4e068249622feb157e3f3b6fa7
sani-yl <saniyonjanlama54@gmail.com>
2026-05-02 03:33:15 +0545

    add build scripts and static file serving

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 
 const app = express()
 app.use(cors())
@@ -39,6 +40,11 @@ app.post('/api/persons', (req, res) => {
 app.delete('/api/persons/:id', (req, res) => {
   persons = persons.filter(p => p.id !== Number(req.params.id))
   res.status(204).end()
+})
+
+app.use(express.static(path.join(__dirname, 'dist')))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 })
 
 const PORT = process.env.PORT || 3001
